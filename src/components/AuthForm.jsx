3168908286a2b61212aeea199c9084d06e345a20
stopbrother <stopbro0323@naver.com>
2024-09-07 14:04:39 +0900

    add: 회원가입 컴포넌트

---
+++ src/components/AuthForm.jsx
@@ -0,0 +1,12 @@
+const AuthForm = () => {
+  return (
+    <form>
+      <input type="text" name="id" placeholder="아이디" />
+      <input type="password" name="password" placeholder="비밀번호" />
+      <input type="text" id="nickname" placeholder="닉네임" />
+      <button></button>
+    </form>
+  );
+};
+
+export default AuthForm;
