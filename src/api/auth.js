21df00598bc37e4a4dc640db89b7b9c2b03edf34
stopbrother <stopbro0323@naver.com>
2024-09-07 13:53:06 +0900

    add: 회원가입 API요청

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,1 +1,8 @@
+import axios from "axios";
+
 const API_URL = "https://moneyfulpublicpolicy.co.kr";
+
+export const register = async (userData) => {
+  const response = await axios.post(`${API_URL}/register`, userData);
+  return response.data;
+};
