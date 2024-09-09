af50d79f0d08b202f05edd0f6289a0886b33e846
stopbrother <stopbro0323@naver.com>
2024-09-09 15:53:32 +0900

    update: 유저 상태 관리리

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import SignUp from "../pages/SignUp";
 import Login from "../pages/Login";
+import { useState } from "react";
 
 const Router = () => {
+  const [user, setUser] = useState(null);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home user={user} />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
       </Routes>
     </BrowserRouter>
   );
