23d69127ea5c9abc370ca1f0dcd9d2e93d84cf61
stopbrother <stopbro0323@naver.com>
2024-09-09 10:39:25 +0900

    add: Profile 컴포넌트 추가

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,31 @@
-export const Profile = () => {
-  return <div>Profile</div>;
+import { useState } from "react";
+
+export const Profile = ({ user, setUser }) => {
+  const [nickname, setNickname] = useState(user?.nickname || "");
+
+  const handleNicknameChange = (e) => {
+    setNickname(e.target.value);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>프로필 수정</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>닉네임</label>
+            <input
+              type="text"
+              name="nickname"
+              onChange={handleNicknameChange}
+            />
+          </div>
+          <button>프로필 수정</button>
+        </form>
+      </div>
+    </div>
+  );
 };
