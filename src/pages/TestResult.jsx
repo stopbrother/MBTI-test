e2180ac0a2fc6b3fdeffb10120a142bb7e482ce4
stopbrother <stopbro0323@naver.com>
2024-09-10 14:11:22 +0900

    add: mbti 결과 삭제 & 공개상태 변환

--- src/pages/TestResult.jsx
+++ src/pages/TestResult.jsx
@@ -1,5 +1,41 @@
-const TestResult = () => {
-  return <div>TestResult</div>;
+import { useEffect, useState } from "react";
+import { getTestResults } from "../api/testResults";
+import TestResultList from "../components/TestResultList";
+
+const TestResult = ({ user }) => {
+  const [results, setResults] = useState([]);
+  console.log("user", user);
+
+  const fetchResults = async () => {
+    const data = await getTestResults();
+    setResults(data);
+  };
+
+  useEffect(() => {
+    fetchResults();
+  }, []);
+
+  // 결과가 변경될 때 상태 업데이트
+  const handleUpdate = () => {
+    fetchResults();
+  };
+  const handleDelete = () => {
+    fetchResults();
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>모든 테스트 결과</h1>
+        <TestResultList
+          results={results}
+          user={user}
+          onUpdate={handleUpdate}
+          onDelete={handleDelete}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default TestResult;
